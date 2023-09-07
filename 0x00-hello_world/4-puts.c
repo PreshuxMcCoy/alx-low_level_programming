4a780b2392cd9a5184792a1ade6d62d6e14ed985
preshux <preshux@gmail.com>
2023-09-07 06:50:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - display  "whatever is written in the print bracket"
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
