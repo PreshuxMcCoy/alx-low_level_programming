a1834a47b04ad9cd071c5ad34423a8c14043ea68
preshux <preshux@gmail.com>
2023-09-08 06:28:54 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet without q and e
+ *
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'e' && l != 'q')
+			putchar(l);
+	}
+
+	putchar('\n');
+	return (0);
+}
